5c4bb378f6e3c17ed026dc7727224c78fc59eee5
yilang <yilang@zhanghongs-MBP.lan>
2015-03-13 00:14:35 +0800

    Initial Commit

---
+++ jybook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jybook
+//
+//  Created by yilang on 15/3/13.
+//  Copyright (c) 2015年 yilang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jybook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jybook
+//
+//  Created by yilang on 15/3/13.
+//  Copyright (c) 2015年 yilang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
