5beff42706f3ad4a02bedd60c012c6012e2a5c34
longwei <yilang2007lw@gmail.com>
2015-03-25 22:19:51 +0800

    impl book config

--- jybook/BookConfig.h
+++ jybook/BookConfig.h
@@ -7,8 +7,15 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface BookConfig : NSObject
+
+@property (nonatomic) BOOL nightMode;
+@property (nonatomic, strong) NSString *backgroundColor;
+@property (nonatomic) NSUInteger fontSize;
+@property (nonatomic) NSUInteger lineSpacing;
+@property (nonatomic) CGFloat brightness;
 
 - (NSArray *) getBookmarksForBook:(NSString *)bookname;
 - (void) setBookmarks:(NSArray *)bookmarks ForBook:(NSString *)bookname;
--- jybook/BookConfig.m
+++ jybook/BookConfig.m
@@ -9,16 +9,90 @@
 #import "BookConfig.h"
 
 @implementation BookConfig
+@synthesize nightMode = _nightMode;
+@synthesize backgroundColor = _backgroundColor;
+@synthesize fontSize = _fontSize;
+@synthesize lineSpacing = _lineSpacing;
+@synthesize brightness  = _brightness;
 
-- (NSArray *) getBookmarksForBook:(NSString *)bookname {
+- (BOOL)nightMode {
+    if (!_nightMode) {
+        NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+        _nightMode = [user boolForKey:@"nightMode"];
+    }
+    return _nightMode;
+}
+
+- (void)setNightMode:(BOOL)nightMode {
+    _nightMode = nightMode;
+    NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+    [user setBool:nightMode forKey:@"nightMode"];
+}
+
+- (NSString *)backgroundColor {
+    if (!_backgroundColor) {
+        NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+        _backgroundColor = [user objectForKey:@"backgroundColor"];
+    }
+    return _backgroundColor;
+}
+
+- (void)setBackgroundColor:(NSString *)backgroundColor {
+    _backgroundColor = backgroundColor;
+    NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+    [user setObject:backgroundColor forKey:@"backgroundColor"];
+}
+
+- (NSUInteger)fontSize {
+    if (!_fontSize) {
+        NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+        _fontSize = [user integerForKey:@"fontSize"];
+    }
+    return _fontSize;
+}
+
+- (void)setFontSize:(NSUInteger)fontSize {
+    _fontSize = fontSize;
+    NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+    [user setInteger:fontSize forKey:@"fontSize"];
+}
+
+- (NSUInteger)lineSpacing {
+    if (!_lineSpacing) {
+        NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+        _lineSpacing = [user integerForKey:@"lineSpacing"];
+    }
+    return _lineSpacing;
+}
+
+- (void)setLineSpacing:(NSUInteger)lineSpacing{
+    _lineSpacing = lineSpacing;
+    NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+    [user setInteger:lineSpacing forKey:@"lineSpacing"];
+}
+
+- (CGFloat)brightness {
+    if (!_brightness) {
+        NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+        _brightness = [user doubleForKey:@"brightness"];
+    }
+    return _brightness;
+}
+
+- (void)setBrightness:(CGFloat)brightness {
+    _brightness = brightness;
+    NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
+    [user setDouble:brightness forKey:@"brightness"];
+}
+
+- (NSArray *)getBookmarksForBook:(NSString *)bookname {
     NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
     return [user objectForKey:[NSString stringWithFormat:@"bookmark->%@", bookname]];
 }
 
-- (void) setBookmarks:(NSArray *)bookmarks ForBook:(NSString *)bookname {
+- (void)setBookmarks:(NSArray *)bookmarks ForBook:(NSString *)bookname {
     NSUserDefaults *user = [NSUserDefaults standardUserDefaults];
     [user setObject:bookmarks forKey:[NSString stringWithFormat:@"bookmark->%@", bookname]];
 }
-
 
 @end
